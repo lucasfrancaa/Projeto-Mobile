c24a5d2d0d5ba5b197030c3961038afe6c27bb9d
Lucas Pereira <lp@quint.com.br>
2023-06-16 00:54:49 -0300

    ajustes

--- App.js
+++ App.js
@@ -6,13 +6,16 @@ import { Cadastro } from "./screens/Cadastro";
 import { Sobre } from "./screens/Sobre";
 import { TerapiaLista } from "./screens/TerapiaLista";
 import { SobreFabio } from "./screens/SobreFabio";
+import { SobreMaria } from "./screens/SobreMaria";
+import { SobreJulia } from "./screens/SobreJulia";
+import { SobreMauricio } from "./screens/SobreMauricio";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="TerapiaLista">
           <Stack.Screen
             name="Login"
             component={Login}
@@ -32,15 +35,36 @@ export default function App() {
           <Stack.Screen
             name="TerapiaLista"
             component={TerapiaLista}
-            options={{ title: "TerapiaLista" }}
+            options={{ title: " " }}
           />
 
           <Stack.Screen
             name="SobreFabio"
             component={SobreFabio}
-            options={{ title: "SobreFabio" }}
+            options={{ title: " " }}
           />
+
+          <Stack.Screen
+            name="SobreMaria"
+            component={SobreMaria}
+            options={{ title: " " }}
+          />      
+
+          <Stack.Screen
+            name="SobreJulia"
+            component={SobreJulia}
+            options={{ title: " " }}
+          />
+
+          <Stack.Screen
+            name="SobreMauricio"
+            component={SobreMauricio}
+            options={{ title: " " }}
+          />        
+
         </Stack.Navigator>
+
+        
       </NavigationContainer>
   );
 }
--- screens/TerapiaLista.js
+++ screens/TerapiaLista.js
@@ -1,66 +1,135 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet, Pressable } from 'react-native';
+import { 
+    View, 
+    Text, 
+    FlatList, 
+    StyleSheet, 
+    Pressable, 
+    SafeAreaView,
+    ScrollView,
+    StatusBar } from 'react-native';
 
 
 export function TerapiaLista({navigation}){
 
-    const terapeutas1 =[
+    const terapeutas1 = [
     {
         id: '01',
         desc: ['Fábio Andrade', 'depressão e suicídio']
-    }
+    },
 ]
 
-    const terapeutas2 =[
+const terapeutas2 = [
     {
         id: '01',
         desc: ['Maria Auxiliadora', 'questões raciais, autoestima.']
-    }
+    },
 ]
 
+const terapeutas3 =[
+    {
+        id: '01',
+        desc: ['Júlia Benzaquen', 'bipolaridade, tdah e transtornos em geral.']
+    },
+]
+
+const terapeutas4 =[
+    {
+        id: '01',
+        desc: ['Maurício Sardá', 'identidade de gênero, lgbtqia+, sexualidade, relacionamentos, preconceito.']
+    },
+
+]
+
+
+
     return(
+        <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
         <View>
             <FlatList data={terapeutas1} keyExtractor={item=>item.id}
                 renderItem={({item})=>
                 <View style={styles.item}><Text style={styles.descricao}>Nome: {item.desc[0]}  Especialidades: {item.desc[1]}</Text>
-                <Pressable onPress={() => navigation.navigate('SobreFabio')}>
-                <Text>saiba mais</Text>
-          </Pressable></View>
+                <Pressable style={styles.botaoSaibaMais} onPress={() => navigation.navigate('SobreFabio')}>
+                <Text style={styles.textoBotaoSaibaMais}>saiba mais</Text>
+            </Pressable></View>
+                
+            }/>
 
-            }
-            
-            />
             <FlatList data={terapeutas2} keyExtractor={item=>item.id}
-            <View style={styles.bemVindo}>
-                <Text>
-                Bem-vindo, ! 
-                </Text>
-            </View>
-            <FlatList data={terapeutas} keyExtractor={item=>item.id}
                 renderItem={({item})=>
                 <View style={styles.item}><Text style={styles.descricao}>Nome: {item.desc[0]}  Especialidades: {item.desc[1]}</Text>
-                <Pressable onPress={() => navigation.navigate('Cadastro')}>
-                <Text>cadastrar</Text>
-          </Pressable></View>
+                <Pressable style={styles.botaoSaibaMais} onPress={() => navigation.navigate('SobreMaria')}>
+                <Text style={styles.textoBotaoSaibaMais}>saiba mais</Text>
+            </Pressable></View>
+                
+            }/>
 
-            }
+            <FlatList data={terapeutas3} keyExtractor={item=>item.id}
+                renderItem={({item})=>
+                <View style={styles.item}><Text style={styles.descricao}>Nome: {item.desc[0]}  Especialidades: {item.desc[1]}</Text>
+                <Pressable style={styles.botaoSaibaMais} onPress={() => navigation.navigate('SobreJulia')}>
+                <Text style={styles.textoBotaoSaibaMais}>saiba mais</Text>
+            </Pressable></View>
+                
+            }/>
+
+            <FlatList data={terapeutas4} keyExtractor={item=>item.id}
+                renderItem={({item})=>
+                <View style={styles.item}><Text style={styles.descricao}>Nome: {item.desc[0]}  Especialidades: {item.desc[1]}</Text>
+                <Pressable style={styles.botaoSaibaMais} onPress={() => navigation.navigate('SobreMauricio')}>
+                <Text style={styles.textoBotaoSaibaMais}>saiba mais</Text>
+            </Pressable></View>
+                
+            }/>
             
-            />
         </View>
 
-        
-        
-    )
-}
+        </ScrollView>
+    </SafeAreaView>
+    );
+};
 const styles=StyleSheet.create({
+
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+      },
+      scrollView: {
+        backgroundColor: '#DDA0DD',
+        marginHorizontal: 20,
+      },
+
     item: {
         backgroundColor: '#fff',
         padding: 15,
         marginVertical: 8,
-        marginHorizontal: 16
+        marginHorizontal: 16,
+        marginBottom: 20
     },
+
     descricao: {
-        fontSize: 18,
-        color: '#000'
-    }
+        fontSize: 15,
+        color: '#000',
+        
+    },
+
+    botaoSaibaMais: {
+        backgroundColor: '#533D8B',
+        width: '40%',
+        height: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 7,
+        marginTop: 10,
+      },
+
+      textoBotaoSaibaMais:{
+        marginBottom: 0,
+        color: '#fff',
+        fontSize: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 7
+    },
 });
